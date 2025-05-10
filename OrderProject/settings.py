04c0ab5ffdacb55bbd73d2a8677863acc176c167
shimo <shimouniverse0503@gmail.com>
2025-05-10 13:15:57 +0900

    allowed_hostの追加

--- OrderProject/settings.py
+++ OrderProject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-b%9ye88@n-qay9p)czn()=aw9@92q8my757bw2@^_%^3vstpu!
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '85.131.250.9']
 
 
 # Application definition
